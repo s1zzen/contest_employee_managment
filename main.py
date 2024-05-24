6d865099209bc77a4aaae43c2399c8c4f65d94b4
s1zzen <dbaskaktyuzxc@gmail.com>
2024-05-24 15:24:13 +0300

    refac

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from models.db_init import init_db
+from src.models.db_init import init_db
 from src.windows.main_window import MainWindow
 if __name__ == '__main__':
     init_db()
